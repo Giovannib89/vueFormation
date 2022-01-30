cb97d212860ea02165617797e7e9de939d8042a5
Giovanni <brugnoni.giovanni89@gmail.com>
2022-01-30 19:10:42 +0100

    fix solution

--- cafe-with-a-vue/cli-version/src/components/simpleMenu.vue
+++ cafe-with-a-vue/cli-version/src/components/simpleMenu.vue
@@ -1,38 +1,34 @@
 <template>
-  <section class="menu">
-				<h2>Menu</h2>
-				<div v-for="item in Array" :key="item.name" class="menu-item">
-					<img
-						class="menu-item__image"
-						:src="item.image.source"
-						:alt="item.image.alt"
-					/>
-					<div>
-						<h3>{{ item.name }}</h3>
-						<p v-if="item.inStock">En stock</p>
-						<p v-else>En rupture de stock</p>
-						<div>
-							<label for="add-item-quantity"
-								>Quantité : {{ item.quantity }}</label
-							>
-							<input
-								v-model.number="item.quantity"
-								id="add-item-quantity"
-								type="number"
-							/>
-							<button @click="addToShoppingCart(item.quantity)">
-								Ajouter au panier d'achat
-							</button>
-						</div>
-					</div>
-				</div>
-			</section>
+
+	<div class="menu-item">
+		<img
+			class="menu-item__image"
+			:src="item.image.source"
+			:alt="item.image.alt"
+		/>
+		<div>
+			<h3>{{ item.name }}</h3>
+			<p v-if="item.inStock">En stock</p>
+			<p v-else>En rupture de stock</p>
+			<div>
+				<label for="add-item-quantity"
+					>Quantité : {{ item.quantity }}</label
+				>
+				<input
+					v-model.number="item.quantity"
+					id="add-item-quantity"
+					type="number"
+				/>
+				<button @click="addToShoppingCart(item.quantity)">
+					Ajouter au panier d'achat
+				</button>
+			</div>
+		</div>
+	</div>			
 </template>
 <script>
 export default {
   name: 'simpleMenu',
-  props: {
-    data: Array
-  }
+  props: ["addToShoppingCart", "image", "inStock", "name", "quantity"]
 }
 </script>
