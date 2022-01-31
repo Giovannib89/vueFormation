ac2bbf5d19f8c61a5638711230bbadddc8787ee5
Giovanni <brugnoni.giovanni89@gmail.com>
2022-01-31 17:28:48 +0100

    fix exo

--- cafe-with-a-vue/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -1,7 +1,30 @@
 <script>
 export default {
 	name: "MenuItem",
-	props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"]
+	props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"],
+	data() {
+		return {
+			onSale: false
+		}
+	},
+	computed: {
+		generatedPrice() {
+			if (this.onSale) { return(this.price * 0.9).toFixed(2)
+			}else {
+				return this.price
+			}
+		}
+	},
+	beforeMount() {
+		const date = new Date()
+		function numIsPair(n) { return (n & 1) ? false : true;}
+		if (!numIsPair(date.getDate())) {
+			this.onSale = true
+		}
+		console.log('Je vais bientot être monté sur le DOM', date.getDate());
+	}
+	
+	
 }
 </script>
 
@@ -10,7 +33,7 @@ export default {
 		<img class="menu-item__image" :src="image.source" :alt="image.alt" />
 		<div>
 			<h3>{{ name }}</h3>
-			<p>Prix : {{ price }}</p>
+			<p>Prix : {{ generatedPrice }}</p>
 			<p v-if="inStock">En stock</p>
 			<p v-else>En rupture de stock</p>
 			<div>
